2965f8f4b2fb96b427fd85252e929a79d9356e47
Christian Makin <christian.makin@gmail.com>
2022-05-08 19:54:21 -0700

    clarified problem

--- 1_10_ackermanns_function.scm
+++ 1_10_ackermanns_function.scm
@@ -1,10 +1,11 @@
-; Exercise 1.10 Ackermann's function
+; Exercise 1.10 Ackermann's function 
 (define (A m n)
   (cond ((= m 0) (* 2 n))
         ((and (>= m 1) (= n 0)) 0)
 	((and (>= m 1) (= n 1)) 2)
         (else (A (- m 1) (A m (- n 1))))))
 
+;Give precise mathematical definitions for the procedures f, g, and h.
 
 ;(f n) = 2n
 (define (f n) (A 0 n))
@@ -14,9 +15,3 @@
 
 ;(h n) = 2^h(n-1)^2
 (define (h n) (A 2 n))
-
-(define (fact n)
-  (if (= n 1)
-      1
-      (* n (fact (- n 1)))))
-
