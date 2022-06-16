67fba0d4718f157bd41ed19a772175ee2973fe88
Christian Makin <christian.makin@gmail.com>
2022-06-16 22:07:23 +0900

    1.32 updated

--- 1_32_accumulate.scm
+++ 1_32_accumulate.scm
@@ -1,12 +1,6 @@
 ; Exercise 1.32 - Implement a procedure that combines a collection of terms, using some general 
 ; accumulation function.
 
-(define (product term a next b)
-  (if (> a b)
-      1
-      (* (term a)
-         (product term (next a) next b))))
-
 (define (accumulate combiner null-value term a next b)
   (if (> a b)
       null-value
