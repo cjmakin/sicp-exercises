35ca25d69a60ecc53bf48a1bec286bfdbf952457
Christian Makin <christian.makin@gmail.com>
2022-06-16 22:54:46 +0900

    1.33 completed

--- 1_33_filtered-accumulate.scm
+++ 1_33_filtered-accumulate.scm
@@ -1,10 +1,8 @@
-; Exercise 1.33: Accumulate procedure with filter.
-
-(define (accumulate combiner null-value term a next b)
-  (if (> a b)
-      null-value
-      (combiner (term a)
-                (accumulate combiner null-value term (next a) next b))))
+; Exercise 1.33: Implement a variation of accumulate that specifies a filter.
+; Show how to epress the following using filtered-accumulate:
+;     a. The sum of squares of the primes numbers in the interval a to b.
+;     b. The product of all positive integers less than n that are relatively 
+;         prime to n.
 
 (define (filter-accumulate filter combiner null-value term a next b)
   (cond ((> a b) null-value)
@@ -18,8 +16,33 @@
 
 (define (identity x) x)
 
-(define (sum-evens a b)
-  (filter-accumulate even? + 0 identity a inc b))
+; ----- Prime check procedure + helpers -----
+(define (smallest-divisor n) (find-divisor n 2))
 
-(sum-evens 1 20)
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
 
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+; ----- Relative prime check + helpers ------
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+
+(define (relatively-prime? a b)
+  (if (= (gcd a b) 1)
+    #t
+    #f))
+;a
+(define (sum-primes a b)
+  (- (filter-accumulate prime? + 0 identity a inc b) 1))
+
+;b
+(define (product-relative-prime n)
+  (filter-accumulate (lambda (a) (relatively-prime? a n)) * 1 identity 1 inc n))
