32fc1d3013ed1bf455f5475025cb8352424bae67
Christian Makin <christian.makin@gmail.com>
2022-07-02 20:00:50 -0700

    1.41 completed

--- 1_41_double-procedure.scm
+++ 1_41_double-procedure.scm
@@ -1,4 +1,4 @@
-; Exercise 1.41 Define a procedure 'double' that takes a procedure of one argument
+; Exercise 1.41 - Define a procedure 'double' that takes a procedure of one argument
 ; as argument and returns a procedure that applies the original procedure twice.
 
 (define (inc x) (+ x 1))
