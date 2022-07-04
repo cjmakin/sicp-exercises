4c21c0b1650a429e6bdf18249def635bf11a1210
Christian Makin <christian.makin@gmail.com>
2022-07-03 18:22:41 -0700

    1.36 edited

--- 1_36_fixed-point-average-damping.scm
+++ 1_36_fixed-point-average-damping.scm
@@ -1,13 +1,14 @@
-;; Exercise 1.36:
-; Modify fixed-point so that it prints the sequence of approximations it generates.
+; Exercise 1.36: -Modify fixed-point so that it prints the sequence of approximations it generates.
 ; Then find a solution to x^x = 1000 by finding a fixed point of x -> log(1000)/ log(x).
-;
 ; Compare the number of steps this takes with and without average damping.
 
 (define tolerance 0.00001)
 
 (define (average a b)
   (/ (+ a b) 2))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
 
 (define (fixed-point f first-guess)
   (define (close-enough? v1 v2)
@@ -22,12 +23,13 @@
           (try next))))  
   (try first-guess))
 
-; With average damping. Number of guesses: 8
-(fixed-point (lambda (x) (average x (/ (log 1000) (log x))))
-             2.0)
+(define (f x)
+  (/ (log 1000) (log x)))
+
+; With average damping. Number of guesses: 9
+(fixed-point (average-damp f) 2.0) 
 
 (newline)
 
 ; Without. Number of guesses: 33
-(fixed-point (lambda (x) (/ (log 1000) (log x)))
-             2.0)
+(fixed-point f 2.0)
